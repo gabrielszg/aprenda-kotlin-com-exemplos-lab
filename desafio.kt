f284e4f441b298614403fee617a93d83bddee0fa
Gabriel Gonçalves <87232582+gabrielszg@users.noreply.github.com>
2022-11-25 11:56:50 -0300

    implementado classes e metodos

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val sobrenome: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
+    
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    // Criacao de usuarios
+    val usuario1 = Usuario("Gabriel", "Souza")
+    val usuario2 = Usuario("Carlos", "Neto")
+    val usuario3 = Usuario("Ana", "Paula")
+    val usuario4 = Usuario("Maria", "Silva")
+    
+    // Criacao de conteudos
+    val conteudo1 = ConteudoEducacional("Conhecendo o Kotlin e Sua Documentação Oficial", 2, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Introdução Prática a Linguagem de Programação Kotlin", 2, Nivel.BASICO)
+    val conteudo3 = ConteudoEducacional("Conhecendo o Java", 2, Nivel.BASICO)
+    val conteudo4 = ConteudoEducacional("Introdução Prática a Linguagem de Programação Java", 2, Nivel.BASICO)
+    val listaConteudoEducacionalKotlin = mutableListOf(conteudo1, conteudo2)
+    val listaConteudoEducacionalJava = mutableListOf(conteudo3, conteudo4)
+    
+    // Criacao de formacoes
+    val kotlin = Formacao("Kotlin Experience", Nivel.BASICO, listaConteudoEducacionalKotlin)
+    val java = Formacao("Java Experience", Nivel.INTERMEDIARIO, listaConteudoEducacionalJava)
+    
+    // Matricula usuario na formacao kotlin
+    kotlin.matricular(usuario1)
+    kotlin.matricular(usuario2)
+    
+    // Matricula usuario na formacao java
+    java.matricular(usuario3)
+    java.matricular(usuario4)
+    
+    // Impressão
+    println("==Formacao ${kotlin.nome}==")
+    println("Conteudos da Formacao:")
+    kotlin.conteudos.forEach { println("- $it") }
+    println()
+    println("Usuarios inscritos na formacao ${kotlin.nome}:")
+    kotlin.inscritos.forEach { println("- $it") }
+    
+    println()
+    
+    println("==Formacao ${java.nome}==")
+    println("Conteudos da Formacao:")
+    java.conteudos.forEach { println("- $it") }
+    println()
+    println("Usuarios inscritos na formacao ${kotlin.nome}:")
+    java.inscritos.forEach { println("- $it") }
 }
